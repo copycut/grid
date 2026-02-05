506acbf5beac960d8e3c2059160d4ff47663a6c4
Didier Chartrain <copycut.design@gmail.com>
2026-02-05 13:43:15 +0100

    feat(board): delete modal shortcut

--- app/components/BoardDeleteModal.tsx
+++ app/components/BoardDeleteModal.tsx
@@ -1,5 +1,6 @@
-import { Board } from '@/lib/supabase/models'
-import { Modal } from 'antd'
+import { Button, Modal } from 'antd'
+import { useKeyboardShortcut } from '@/lib/hooks/useKeyboardShortcut'
+import ShortcutIndicator from '@/app/components/ui/ShortcutIndicator'
 
 export default function BoardDeleteModal({
   isOpen,
@@ -12,14 +13,28 @@ export default function BoardDeleteModal({
   onClose: () => void
   onSubmit: (boardId: number) => void
 }) {
+  useKeyboardShortcut(() => boardId && onSubmit(boardId), {
+    key: 'Enter',
+    modifiers: { cmdOrCtrl: true },
+    enabled: isOpen,
+    preventDefault: true
+  })
+
   return (
     <Modal
       title="Delete Board"
       open={isOpen}
       onOk={() => boardId && onSubmit(boardId)}
-      okText="Delete"
       onCancel={onClose}
-      okType="danger"
+      footer={[
+        <Button key="cancel" onClick={onClose}>
+          Cancel
+        </Button>,
+        <Button key="submit" danger onClick={() => boardId && onSubmit(boardId)}>
+          <ShortcutIndicator color="red">⏎</ShortcutIndicator>
+          Delete
+        </Button>
+      ]}
     >
       <p>Are you sure you want to delete this board? This action is irreversible.</p>
     </Modal>
--- app/components/ui/ShortcutIndicator.tsx
+++ app/components/ui/ShortcutIndicator.tsx
@@ -1,13 +1,19 @@
 import { Tag } from 'antd'
 
-export default function ShortcutIndicator({ children }: { children: React.ReactNode }) {
+export default function ShortcutIndicator({
+  children,
+  color = 'white'
+}: {
+  children: React.ReactNode
+  color?: string
+}) {
   // detect if the user is on mac or windows to display the correct shortcut
   const isMac = () => {
     return /Mac|iPhone|iPad|iPod/.test(navigator.userAgent)
   }
 
   return (
-    <Tag variant="outlined" color="white" className="bg-transparent! opacity-50!">
+    <Tag variant="outlined" color={color} className="bg-transparent! opacity-50!">
       <span className="text-xs">{isMac() ? '⌘' : 'Ctrl'}</span>
       {children}
     </Tag>
